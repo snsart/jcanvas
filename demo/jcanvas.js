e5ead9c4789870f2475f46b78b69a63d65a380f8
snsart <snsart@163.com>
2019-05-07 17:38:17 +0800

    add

--- demo/jcanvas.js
+++ demo/jcanvas.js
@@ -1,10 +1,26 @@
+this.jcanvas = this.jcanvas||{};
+
+jcanvas.extend = function(subclass, superclass) {
+	"use strict";
+	function o() { this.constructor = subclass; }
+	o.prototype = superclass.prototype;
+	return (subclass.prototype = new o());
+};
+
+
 function DisplayObject(){
 	
 }
 
-function Stage(canvas){
+
+(function(){
+	function Stage(canvas){
+		
+	}
 	
-}
+	
+})()
+
 
  /**
  * 容器
@@ -17,14 +33,65 @@ function Container(){
  /**
  * 形状
  */
-
-function Shape(){
+(function(){
+	function Shape(){
 	
-}
+	}	
+})()
+
 
  /**
  * 绘图
  */
-function Graphics(){
+(function(){
+	function Graphics(){
 	
-}
+	}
+	var G=Graphics,p=Graphics.prototype;
+	p.instructions=[];
+	
+	p.lineTo=function(x,y){
+		p.instructions.push(new G.LineTo(x,y));
+	};
+	p.moveTo=function(x,y){
+		p.instructions.push(new G.MoveTo(x,y));
+	}
+	p.lineStyle=function(thick,color){
+		p.instructions.push(new G.LineStyle(thick,color));
+	};	
+	p.endStroke=function(){
+		p.instructions.push(new G.EndStroke());
+	};
+	p.draw=function(ctx){
+		console.log(ctx);
+		for(var i=0;i<this.instructions.length;i++){
+			this.instructions[i].exec(ctx);
+		}
+	};
+	
+	(G.LineTo=(function(x,y){
+		this.x=x;
+		this.y=y;
+	})).prototype.exec=function(ctx){ctx.lineTo(this.x,this.y)};
+	
+	(G.MoveTo=(function(x,y){
+		this.x=x;
+		this.y=y;
+	})).prototype.exec=function(ctx){console.log(ctx);ctx.moveTo(this.x,this.y)};
+	
+	(G.LineStyle=(function(thick,color){
+		this.thick=thick;
+		this.color=color;
+	})).prototype.exec=function(ctx){
+		console.log(ctx)
+		ctx.fillStyle=this.color;
+		ctx.lineWidth=this.thick;
+	};
+	
+	(G.EndStroke=(function(){
+	})).prototype.exec=function(ctx){ctx.stroke()};
+	
+	this.jcanvas.Graphics=Graphics;
+	
+})()
+
--- demo/test.js
+++ demo/test.js
@@ -1,4 +1,22 @@
-canvas = document.getElementById("canvas");
+var canvas = document.getElementById("canvas");
+var ctx=canvas.getContext('2d');
+var g=new jcanvas.Graphics();
+/*ctx.fillStyle = '#dddddd';
+ctx.lineWidth=10;
+ctx.strokeStyle = '#0000ff';
+ctx.moveTo(0,0);
+ctx.lineTo(200,200);
+ctx.stroke();*/
+console.log(ctx);
+
+g.lineStyle(15, 0xffffff);
+g.moveTo(0,0);
+g.lineTo(200,200);
+g.endStroke();
+g.draw(ctx);
+
+/*console.log(14);
+var canvas = document.getElementById("canvas");
 var stage=new Stage(canvas);
 
 var line=new Shape();
@@ -7,8 +25,65 @@ stage.addChild(line);
 line.graphics.lineStyle(2, 0xffffff);
 line.graphics.moveTo(0, 0);
 line.graphics.lineTo(0, 117);
-line.graphics.endFill();
+line.graphics.endStroke();
 line.x = 172;
-line.y = 61;
+line.y = 61;*/
+
+stage.draw();
+
+function Ball(radius,color){
+	if(radius===undefined){radius=40;}
+	if(color===undefined){color="#ff0000";}
+	this.x=0;
+	this.y=0;
+	this.vx=0;
+	this.vy=0;
+	this.mass=1;
+	this.radius=radius;
+	this.rotation=0;
+	this.scaleX=1;
+	this.scaleY=1;
+	this.color=utils.parseColor(color,1);
+	this.lineWidth=1;
+}
+
+Ball.prototype.draw=function(context){
+	context.save();
+	context.translate(this.x,this.y);
+	context.rotate(this.rotation);
+	context.scale(this.scaleX,this.scaleY);
+	context.lineWidth=this.lineWidth;
+	context.fillStyle=this.color;
+	context.beginPath();
+	context.arc(0,0,this.radius,0,(Math.PI*2),true);
+	context.closePath();
+	context.fill();
+	if(this.lineWidth>0){
+		context.stroke();
+	}
+	context.restore();
+};
+
+/*var canvas = document.getElementById('canvas'),
+width = canvas.width,
+height = canvas.height,
+ctx = canvas.getContext('2d');
+
+ctx.clearRect(0, 0, 200, 200); // 擦除(0,0)位置大小为200x200的矩形，擦除的意思是把该区域变为透明
+ctx.fillStyle = '#dddddd'; // 设置颜色
+ctx.fillRect(10, 10, 130, 130); // 把(10,10)位置大小为130x130的矩形涂色
+// 利用Path绘制复杂路径:
+var path=new Path2D();
+path.arc(75, 75, 50, 0, Math.PI*2, true);
+path.moveTo(110,75);
+path.arc(75, 75, 35, 0, Math.PI, false);
+path.moveTo(65, 65);
+path.arc(60, 65, 5, 0, Math.PI*2, true);
+path.moveTo(95, 65);
+path.arc(90, 65, 5, 0, Math.PI*2, true);
+ctx.strokeStyle = '#0000ff';
+ctx.stroke(path);*/
+
+
 
 
